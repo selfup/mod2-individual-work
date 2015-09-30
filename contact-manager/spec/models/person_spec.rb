394351d7d8c11b105449c18270ad616247510e3d
Regis <boudinot.regis@yahoo.com>
2015-09-30 09:32:38 -0600

    Contact manager

---
+++ contact-manager/spec/models/person_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+
+
+describe Person, type: :model do
+
+  let(:person) do
+    Person.new(first_name: 'Alice', last_name: 'Smith')
+  end
+
+  it "is invalid without a first name" do
+    person.first_name = nil
+    expect(person).to_not be_valid
+  end
+
+  it "is invalid without a last name" do
+    person.last_name = nil
+    expect(person).to_not be_valid
+  end
+
+  it 'is valid' do
+    expect(person).to be_valid
+  end
+
+  it "has an array of phone numbers" do
+    expect(person.phone_numbers).to eq([])
+  end
+
+end
